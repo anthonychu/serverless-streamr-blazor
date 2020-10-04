bee0e1b3263f08c43e3cf5e742122d4a84f3f313
Anthony Chu <antchu@microsoft.com>
2020-10-04 10:12:57 -0700

    Add function app, camera.js

---
+++ Api/Functions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace ServerlessStreamR.Api
+{
+    public static class Functions
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "streamrblazor")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
