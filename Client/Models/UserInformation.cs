65a8de2de09e9c8bd535e2237390bd5229286108
Anthony Chu <antchu@microsoft.com>
2020-10-05 00:31:47 -0700

    Update user info structure

--- Client/Models/UserInformation.cs
+++ Client/Models/UserInformation.cs
@@ -2,6 +2,12 @@ namespace ServerlessStreamR.Client.Models
 {
     public class UserInformation
     {
+        public ClientPrincipal ClientPrincipal { get; set; }
+    }
+
+    public class ClientPrincipal
+    {
+        public string IdentityProvider { get; set; }
         public string UserDetails { get; set; }
     }
 }
